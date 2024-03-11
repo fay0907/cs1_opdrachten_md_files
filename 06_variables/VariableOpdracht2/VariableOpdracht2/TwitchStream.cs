87f3af6e1e61009fecd0c9602225df06244be4ed
progsen <>
2024-03-11 16:24:56 +0100

    small fix

--- 06_variables/VariableOpdracht2/VariableOpdracht2/TwitchStream.cs
+++ 06_variables/VariableOpdracht2/VariableOpdracht2/TwitchStream.cs
@@ -4,7 +4,14 @@ namespace VariableOpdracht2
 {
     internal class TwitchStream
     {
-        //maak hier class variables aan:
+        /*
+        - maak hier class variables aan
+        - Zet overal internal voor
+        -  LET OP, geef hier geen waardes. dus alleen : 
+            > internal int mijnVariable;
+        */
+        
+        //maak deze variabelen aan:
         // type=string, naam: name,
         // type=string, naam: lastStreamName
         // type=Game, naam: streamGame
