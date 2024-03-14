9937d421f01b1cce9a8081d01102b44489ab5e19
progsen <>
2024-03-14 15:01:52 +0100

    video foutje

--- 07_rekenen/CalculateOpdracht1/CalculateOpdracht1/Program.cs
+++ 07_rekenen/CalculateOpdracht1/CalculateOpdracht1/Program.cs
@@ -10,18 +10,18 @@ namespace CalculateOpdracht1
 
             int leeftijdStudent = 16;
 
-            //nu ben je jarig, tel 1 bij je leeftijd op, gebruik de = A+1; constructie (zie video)
+            //nu ben je jarig, tel 1 bij je leeftijd op, gebruik de variable = variabel +1; constructie 
             Console.WriteLine("Year 1 Happy birthday!");
             Console.WriteLine($"je bent nu {leeftijdStudent}" );
 
 
 
-            //nu ben je weer jarig, tel 1 bij je leeftijd op, gebruik de += 1; constructie (zie video)
+            //nu ben je weer jarig, tel 1 bij je leeftijd op, gebruik de += 1; constructie 
             Console.WriteLine("Year 2 Happy birthday!");
             Console.WriteLine($"je bent nu {leeftijdStudent}");
 
 
-            //nu ben je weer jarig, tel 1 bij je leeftijd op, gebruik de ++; constructie (zie video)
+            //nu ben je weer jarig, tel 1 bij je leeftijd op, gebruik de ++; constructie
             Console.WriteLine("Year 3 Happy birthday!");
             Console.WriteLine($"je bent nu {leeftijdStudent}");
 
